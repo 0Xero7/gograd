a331b5acf68457b407cda9bcc1a3d10cec357fb8
0Xero7 <smpsmp13@gmail.com>
2025-01-22 02:56:32 +0530

    optimize optimizer

--- optimizers/adam_tensor.go
+++ optimizers/adam_tensor.go
@@ -9,7 +9,7 @@ type AdamTensor struct {
 	lr           float64
 	beta1, beta2 float64
 	epsilon      float64
-	m, v         map[*ng.Tensor][]float64
+	m, v         map[int][]float64
 	t            int
 
 	effectiveBeta1, effectiveBeta2 float64
@@ -21,8 +21,8 @@ func NewAdamTensor(lr float64) *AdamTensor {
 		beta1:   0.9,   // Exponential decay rate for first moment
 		beta2:   0.999, // Exponential decay rate for second moment
 		epsilon: 1e-8,  // Small constant to prevent division by zero
-		m:       make(map[*ng.Tensor][]float64),
-		v:       make(map[*ng.Tensor][]float64),
+		m:       make(map[int][]float64),
+		v:       make(map[int][]float64),
 		t:       0,
 
 		effectiveBeta1: 0.9,
@@ -51,37 +51,47 @@ func (a *AdamTensor) Step(params []*ng.Tensor) {
 	a.t++
 	a.effectiveBeta1 *= a.beta1
 	a.effectiveBeta2 *= a.beta2
+
+	mHatFactor := 1.0 / (1.0 - a.effectiveBeta1)
+	vHatFactor := 1.0 / (1.0 - a.effectiveBeta2)
 	// mHatD := a.beta1 * 0.9   // binPow(a.beta1, a.t)
 	// vHatD := a.beta2 * 0.999 // binPow(a.beta2, a.t)
 
-	for _, param := range params {
-		if param == nil || !param.RequiresOptimization {
+	for _, paramx := range params {
+		if paramx == nil || !paramx.RequiresOptimization {
 			continue
 		}
+		param := paramx.Id
 
 		// grad := param.Grad
 
 		// Initialize momentum and velocity if not present
 		if _, exists := a.m[param]; !exists {
-			a.m[param] = make([]float64, param.Len())
-			a.v[param] = make([]float64, param.Len())
+			a.m[param] = make([]float64, paramx.Len())
+			a.v[param] = make([]float64, paramx.Len())
 		}
 
-		for index := range param.Len() {
+		for index := range paramx.Len() {
+			am := a.m[param][index]
+			av := a.v[param][index]
+			pg := paramx.Grad[index]
+
 			// Update biased first moment estimate
-			a.m[param][index] = a.beta1*a.m[param][index] + (1-a.beta1)*param.Grad[index]
+			a.m[param][index] = a.beta1*am + (1-a.beta1)*pg
+			am = a.m[param][index]
 
 			// Update biased second moment estimate
-			a.v[param][index] = a.beta2*a.v[param][index] + (1-a.beta2)*param.Grad[index]*param.Grad[index]
+			a.v[param][index] = a.beta2*av + (1-a.beta2)*pg*pg
+			av = a.v[param][index]
 
 			// Compute bias-corrected first moment estimate
-			mHat := a.m[param][index] / (1 - math.Pow(a.beta1, float64(a.t)))
+			mHat := am * mHatFactor
 
 			// Compute bias-corrected second moment estimate
-			vHat := a.v[param][index] / (1 - math.Pow(a.beta2, float64(a.t)))
+			vHat := av * vHatFactor
 
 			// Update parameters
-			param.Value[index] -= a.lr * mHat / (math.Sqrt(vHat) + a.epsilon)
+			paramx.Value[index] -= a.lr * mHat / (math.Sqrt(vHat) + a.epsilon)
 		}
 	}
 }
